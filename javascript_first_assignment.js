31801198403155e0dc77681428ff625a9237acc8
Rasheedat-Ajidagba95 <140977388+Rasheedat-Ajidagba95@users.noreply.github.com>
2025-02-27 20:14:56 +0100

    Assignment

---
+++ javascript_first_assignment.js
@@ -0,0 +1,18 @@
+// Declare two variables
+
+let num1 = 5; 
+let num2 = 10;
+
+// Perform operations and store results
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+// Print results
+   console.log("Addition: " + num1 + " + " + num2 + " = " + addition);
+   console.log("Subtraction: " + num1 + " - " + num2 + " = " + subtraction);
+   console.log("multiplication: " + num1 + " * " + num2 + " = " + multiplication);
+   console.log("division: " + num1 + " / " + num2 + " = " + division);
+   console.log("modulus: " + num1 + " % " + num2 + " = " + modulus);
